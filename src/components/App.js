f60d1969e0e26bc47e78ec3ef75546b6ca04d816
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-31 21:22:44 +0530

    Setting up the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+
+  }
+
+  handleChange(event){
+    event.preventDefault();
+    //console.log(event.target.value);
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  handleKeyUp(event){
+    if(event.key === 'Enter' && this.state.searchTerm !== ''){
+      let searchUrl = "search/multi?query=" + this.state.searchTerm + "&apikey=" + 'af43b44e676c68ef2bc426078345b150';
+      this.setState({searchUrl:searchUrl});
+    }
+  }
+
   render() {
     return (
-      <div className="header">
+      <div>
+        <header className="header">
+          Netflix
+          <div id="search" className="Search">
+            <input 
+              type="search" 
+              placeholder="Search for a title..."
+              onChange={this.handleChange}
+              value={this.state.searchTerm}
+              onKeyUp={this.handleKeyUp}
+            /> 
+          </div>
+        </header>
       
       </div>
     );
